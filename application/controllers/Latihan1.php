9d5e822ee62965e0dc2716dede37f2e2edc6b3fa
Ganige <muhamad.mag@bsi.ac.id>
2022-03-17 11:38:51 +0700

    revisi index.php/Latihan1.php

--- application/controllers/Latihan1.php
+++ application/controllers/Latihan1.php
@@ -7,15 +7,5 @@ class Latihan1 extends CI_Controller
         //$this->load ->view (view-latihan1');
     }
 
-    public function penjumlahan ($n1,$n2)
-    {
-        $this->load->model ('Model_Latihan1');
-        $data['nilai1']=$n1;
-        $data['nilai2']=$n2;
-        $data['hasil']=$this -> Model_Latihan1->jumlah($n1,$n2);
-
-        $this ->load->view ('view-latihan1',$data);
-
-    }
     
 }
